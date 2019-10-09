c36f11d81ced362926ce525f2d0744b6aaa3aa8d
tianhuanhuan <407339135@qq.com>
2019-10-09 17:28:31 +0800

    chaptor01

--- chapter-01/rxjs/timingSenseTest.js
+++ chapter-01/rxjs/timingSenseTest.js
@@ -3,15 +3,22 @@ const mouseDown$ = Rx.Observable.fromEvent(holdMeButton, 'mousedown');
 const mouseUp$ = Rx.Observable.fromEvent(holdMeButton, 'mouseup');
 
 const holdTime$ = mouseUp$.timestamp().withLatestFrom(mouseDown$.timestamp(), (mouseUpEvent, mouseDownEvent) => {
-  return mouseUpEvent.timestamp- mouseDownEvent.timestamp;
+  console.log(11111, mouseUpEvent, mouseDownEvent)
+  const res = mouseUpEvent.timestamp- mouseDownEvent.timestamp;
+  document.querySelector('#hold-time').innerText = res;
+  return res
 });
 
-holdTime$.subscribe(ms => {
-  document.querySelector('#hold-time').innerText = ms;
-});
+// holdTime$.subscribe(ms => {
+//   console.log(22222, ms)
+//   document.querySelector('#hold-time').innerText = ms;
+// });
 
 holdTime$.flatMap(ms => Rx.Observable.ajax('https://timing-sense-score-board.herokuapp.com/score/' + ms))
-.map(e => e.response)
+.map(e => {
+  console.log(33333, e)
+  return e.response
+})
 .subscribe(res => {
   document.querySelector('#rank').innerText = '你超过了' + res.rank + '% 的用户';
 });
